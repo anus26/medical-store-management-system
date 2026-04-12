0f39be03d340d0539d1e63a007fd01c3a9218b74
anus26 <anusraza268@gmail.com>
2026-04-12 08:57:52 -0700

    modi

--- medical_store/medical_store/settings.py
+++ medical_store/medical_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tas*n&+0!xki8$bwa$j@8a8@)k_fn)c))z_#wams4i2_7apylr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.koyeb.app']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'medical_store.urls'
@@ -133,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/admin-dashboard/'   # default redirect
